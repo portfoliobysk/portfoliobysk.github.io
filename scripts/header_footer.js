359c8f1145218e757b278383634df87fec0155e4
portfoliobysk <s-keigo@ymail.ne.jp>
2025-12-25 17:13:50 +0900

    update: css大掃除、topページコーディング

--- scripts/header_footer.js
+++ scripts/header_footer.js
@@ -1,10 +1,12 @@
 const headerContent = `
-    <h1><img src="https://portfoliobysk.github.io/images/logo.svg" alt="S.K's Portfolio Logo"></h1>
+  <h1><img src="https://portfoliobysk.github.io/images/logo.svg" alt="S.K's Portfolio Logo"></h1>
     <nav>
-      <a href="#About" class="anchor">About</a>
-      <a href="#Profile" class="anchor">Profile</a>
-      <a href="#Portfolio" class="anchor">Portfolio</a>
+      <a href="#profile__link">Profile</a>
+      <a href="#skills__link">Skill's</a>
+      <a href="#portfolio__link">Portfolio</a>
+      <a href="#career__link">Career</a>
       <a href="https://github.com/portfoliobysk/">GitHub</a>
+      <a href="https://forms.gle/CALFLhAjRGj49LZC6">Contact</a>
     </nav>
 `;
 
@@ -35,13 +37,16 @@ const breadcrumbContent = () => {
 }
 
 const footerContent = `
-    <small>&#169; 2025 S.K</small>
-    <div id="footer__nav">
-        <a href="#About">About</a>
-        <a href="#Profile">Profile</a>
-        <a href="#Portfolio">Portfolio</a>
-        <a href="https://github.com/portfoliobysk/">GitHub</a>
-    </div>
+  <img src="https://portfoliobysk.github.io/images/logo.svg" alt="S.K's Portfolio Logo">
+  <small>&#169; 2025 S.K</small>
+  <div id="footer__nav">
+    <a href="#profile__link">Profile</a>
+    <a href="#skills__link">Skill's</a>
+    <a href="#portfolio__link">Portfolio</a>
+    <a href="#career__link">Career</a>
+    <a href="https://github.com/portfoliobysk/">GitHub</a>
+    <a href="https://forms.gle/CALFLhAjRGj49LZC6">Contact</a>
+  </div>
 `;
 
 // ページロード後に実行
